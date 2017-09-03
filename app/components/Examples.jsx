c5ef2380d3f86dd12365839e73d7993530e1fd1f
AnteD <antedomio2.net.hr>
2017-09-03 13:00:44 +0200

    add style for main and examples component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component</h3>
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few examples to try out!</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Split'>Split</Link>
+                </li>
+                <li>
+                    <Link to='/?location=London'>London</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
+        </div>
     )
 };
 
